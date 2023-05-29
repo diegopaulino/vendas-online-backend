37dc8d5f0b950425c924a0607dd33604c7587c71
Diego <diegopaulino_3@hotmail.com>
2023-05-28 23:36:03 -0300

    db infos from env file

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -3,11 +3,11 @@ import { UserEntity } from './user/interface/user.entity';
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    database: "vendason",
-    host: "localhost",
-    password: "mysecretpassword",
-    port: 5432,
-    username: "postgres",
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
     entities: [`src/**/*.entity{.js,.ts}`, UserEntity],
     migrations: [`${__dirname}/src/migration/{*.ts,*.js}`],
 });
