7553819551691e07702a7b81eb4b5c325003e47d
Diego <diegopaulino_3@hotmail.com>
2023-06-01 05:10:54 -0300

    feat: create JWT token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,21 +3,31 @@ import { LoginDto } from './dtos/login.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayload } from './dtos/loginPayload.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userService: UserService,
-    ) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-    async login(loginDto: LoginDto): Promise<UserEntity> {
-        const user: UserEntity | undefined = await this.userService.findUserByEmail(loginDto.email).catch(() => undefined);
-        const isMatch = await compare(loginDto.password, user?.password || '');
+  async login(loginDto: LoginDto): Promise<ReturnLogin> {
+    const user: UserEntity | undefined = await this.userService
+      .findUserByEmail(loginDto.email)
+      .catch(() => undefined);
+    const isMatch = await compare(loginDto.password, user?.password || '');
 
-        if(!user || !isMatch) {
-            throw new NotFoundException('Email or password invalid')
-        }
-
-        return user;
+    if (!user || !isMatch) {
+      throw new NotFoundException('Email or password invalid');
     }
+
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayload(user) }),
+      user: new ReturnUserDto(user),
+    };
+  }
 }
